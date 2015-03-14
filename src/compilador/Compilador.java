888616be6f8d4b8c38a894d567ce7a0a8f40bbc3
Zubi <Zubi@10.33.32.98>
2015-03-14 16:46:48 -0700

    Test

--- src/compilador/Compilador.java
+++ src/compilador/Compilador.java
@@ -1,5 +1,7 @@
 package compilador;
 
+import java.util.StringTokenizer;
+
 /**
  *
  * @author Zubi
@@ -9,5 +11,21 @@ public class Compilador {
     private static final int FIN_SENT = ';';
     private static final int CONCAT = '&';
     private static final int ALTER = '|';
+    
+    private Integer linea = 1;
+    private StringTokenizer tokenizer = null;
+    
+     private StringTokenizer getTokenizer (String codigoFuente){
+        if(this.tokenizer == null){
+            String alfabeto = String.format("%S%S", 
+                    (char) CONCAT,
+                    (char) ALTER
+            
+            );
+            this.tokenizer = new StringTokenizer (codigoFuente.trim(),alfabeto, true);
+        }
+        return this.tokenizer;
+        
+    }
 
 }
