a8ca2afa0e293ba4bc6fc23ada6345baca91e086
Zubi <Zubi@10.33.32.98>
2015-03-14 15:26:04 -0700

    Test

---
+++ src/compilador/Compilador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+/**
+ *
+ * @author Zubi
+ */
+public class Compilador {
+
+    private static final int FIN_SENT = ';';
+    private static final int SUMA = '+';
+    private static final int RESTA = '-';
+    private static final int MULTIPLICACION = '*';
+    private static final int DIVISION = '/';
+    private static final int PAR_DER = ')';
+    private static final int PAR_IZQ = '(';
+    private static final int NUMERO = 600;
+    private static final int ID = 700;
+
+}
