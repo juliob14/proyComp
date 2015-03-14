2e05b919a174e4b388db739ad05c25e217a94425
Zubi <Zubi@10.33.32.98>
2015-03-14 16:32:33 -0700

    Test

--- src/compilador/Compilador.java
+++ src/compilador/Compilador.java
@@ -7,13 +7,7 @@ package compilador;
 public class Compilador {
 
     private static final int FIN_SENT = ';';
-    private static final int SUMA = '+';
-    private static final int RESTA = '-';
-    private static final int MULTIPLICACION = '*';
-    private static final int DIVISION = '/';
-    private static final int PAR_DER = ')';
-    private static final int PAR_IZQ = '(';
-    private static final int NUMERO = 600;
-    private static final int ID = 700;
+    private static final int CONCAT = '&';
+    private static final int ALTER = '|';
 
 }
