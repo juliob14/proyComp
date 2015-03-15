3f4670b711399cd6446fee7b2962f92c071a3fc8
Zubi <Zubi@10.33.32.98>
2015-03-14 17:04:47 -0700

    Test

--- src/compilador/Compilador.java
+++ src/compilador/Compilador.java
@@ -1,3 +1,27 @@
+/**
+ * <ul>
+ *<li> Prog →  conjProd</li>
+ * <li> Conjprod→ ConjProd</li>
+ *         |Prod
+ * <li>Prod  → variable DEF Expr;</li>
+ * <li>Expr →  Expr ALT term</li>
+ * <li>Term → term & factor |Factor </li>
+ * <li>Factor {Expr} | [Expr] | primario</li>
+ * <li>Primario → Expr | Variable | Terminal</li>
+ * 
+ * 
+ * A)Concatenacion: caracter &
+ * B) Alternacion: Caracter ' | '
+ * C) Cerradura cero o mas : Se representa con los caracteres ' { } 'Ejemp {<digito>}
+ * D) Cerradura cero o uno se representa por los caracteres ' [ ] ' ejemp [+|-] 
+ * significa que se puede tomar el caracter + o - de manera opcional
+ * E)Definicion: Se representa por la secuencia de caracteres  ' ::= '
+ * F)Fin de produccion: Caracter ' ; ' 
+ * G)fin de archivo: caracter '.'
+ * </ul>
+ * @author Carlos Zubiate
+ */
+
 package compilador;
 
 import java.util.StringTokenizer;
